ced3dd6debc7f65220bb12a417f6d1808da0f481
Ryota Ohhashi <ryota.ohhashi@accenture.com>
2022-06-23 15:14:35 +0900

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -6,60 +6,62 @@ const sketch = (p5) => {
   // let drawingTime = 1;
   // let time = 0;
   // let length = 50;
+  let deleteIndex = [];
   let draw = [];
   let mouseOver = false;
   let isDrawing = false;
   let mouse = { x: 0, y: 0 };
+  let windowWidth = p5.windowWidth;
+  let windowHeight = p5.windowHeight;
 
   p5.preload = () => {
     img = p5.loadImage("/img/img1.jpg");
   }
 
   p5.setup = () => {
-    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    p5.createCanvas(windowWidth, windowHeight);
     p5.smooth();
-    p5.image(img, p5.windowWidth/2 - 250, p5.windowHeight/2 - 250, 500, 500);
+    p5.image(img, windowWidth/2 - 250, windowHeight/2 - 250, 500, 500);
 
-    const canvas = document.querySelector('canvas');
-
-    canvas.addEventListener('mouseover', () => {
-      mouseOver = true;
-    });
-    canvas.addEventListener('mouseout', () => {
-      mouseOver = false;
-    });
     window.addEventListener('mousemove', (e) => {
       mouse.x = e.clientX;
       mouse.y = e.clientY;
+      if (mouse.x > windowWidth/2 - 250 && mouse.x < windowWidth/2 + 250 && mouse.y > windowHeight/2 - 250 && mouse.y < windowHeight/2 + 250) {
+        mouseOver = true;
+      }else{
+        mouseOver = false;
+      }
     });
   };
 
   p5.draw = () => {
-    // マウスの位置の色を取得
-    const color = p5.get(mouse.x, mouse.y);
-    const r = color[0];
-    const g = color[1];
-    const b = color[2];
+    if (mouseOver) {
+      // マウスの位置の色を取得
+      const color = p5.get(mouse.x, mouse.y);
+      const r = color[0];
+      const g = color[1];
+      const b = color[2];
 
-    // 極端な黒白系の色は追加しない
-    if (judgeColor(r, g, b)){
-      // 長さの決定
-      length = p5.random(50, 500);
-      // 追加
-      draw.push(
-        {
-          mouseX: mouse.x,
-          mouseY: mouse.y,
-          r: r,
-          g: g,
-          b: b,
-          length: mouse.y + length
-        }
-      );
+      // 極端な黒白系の色は追加しない
+      if (judgeColor(r, g, b)){
+        // 長さの決定
+        length = p5.random(50, 500);
+        // 追加
+        draw.push(
+          {
+            mouseX: mouse.x,
+            mouseY: mouse.y,
+            r: r,
+            g: g,
+            b: b,
+            length: mouse.y + length
+          }
+        );
+      }
     }
 
     // 描画
-    draw.forEach((d) => {
+    draw.forEach((d, i) => {
       if (d.mouseY < d.length) {
         drawLine(p5, d.mouseX, d.mouseY, d.r, d.g, d.b, 5);
         d.mouseY += 5;
@@ -68,8 +70,13 @@ const sketch = (p5) => {
           d.g += 3;
           d.b += 3;
         }
+      }else{
+        deleteIndex.push(i);
       }
     });
+    // 描画が終わったものを削除
+    draw = draw.filter((d, index) => !deleteIndex.includes(index));
+    deleteIndex = [];
   };
 
   function drawLine(p5, x, y, r, g, b, length) {
@@ -78,11 +85,13 @@ const sketch = (p5) => {
     p5.stroke(r, g, b);
     p5.line(x, y, x, y + length);
   }
+
   function judgeColor(r, g, b){
     if(r < 20 && g < 20 && b < 20) return false;
     if(r > 230 && b > 230 && g > 230) return false;
     return true
   };
+
 };
 
 new p5(sketch);
